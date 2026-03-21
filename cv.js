12a7df6884a7ec185298798d0a1d2620e35b5e11
CodigyX <zS21004533@estudiantes.uv.mx>
2026-03-21 17:42:59 -0600

    Actualización CV 3/3

--- cv.js
+++ cv.js
@@ -2,16 +2,25 @@ document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("btnPDF");
 
   btn.addEventListener("click", () => {
+
+    const body = document.getElementById("body");
     const element = document.getElementById("cv");
 
+    // ACTIVAR MODO ATS
+    body.classList.add("ats-mode");
+
     const opt = {
-      margin: 0.2,
-      filename: "Erick_Barrena_Backend_Developer.pdf",
-      image: { type: 'jpeg', quality: 1 },
-      html2canvas: { scale: 3, useCORS: true },
-      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+      margin: 0.3,
+      filename: "Erick_Barrena_Backend_Developer_ATS.pdf",
+      image: { type: "jpeg", quality: 1 },
+      html2canvas: { scale: 2, useCORS: true },
+      jsPDF: { unit: "in", format: "letter", orientation: "portrait" }
     };
 
-    html2pdf().set(opt).from(element).save();
+    html2pdf().set(opt).from(element).save().then(() => {
+      // DESACTIVAR MODO ATS
+      body.classList.remove("ats-mode");
+    });
+
   });
 });
