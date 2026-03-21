5f242b02d3c68b1ec676a86dddf559856dbc2d8f
CodigyX <zS21004533@estudiantes.uv.mx>
2026-03-21 15:55:09 -0600

    Actualización CV 2/3

--- cv.js
+++ cv.js
@@ -1,13 +1,17 @@
-function downloadPDF() {
-  const element = document.getElementById("cv");
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("btnPDF");
 
-  const opt = {
-    margin: 0.2,
-    filename: "Erick_Barrena_Backend_Developer.pdf",
-    image: { type: 'jpeg', quality: 1 },
-    html2canvas: { scale: 3 },
-    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
-  };
+  btn.addEventListener("click", () => {
+    const element = document.getElementById("cv");
 
-  html2pdf().set(opt).from(element).save();
-}
+    const opt = {
+      margin: 0.2,
+      filename: "Erick_Barrena_Backend_Developer.pdf",
+      image: { type: 'jpeg', quality: 1 },
+      html2canvas: { scale: 3, useCORS: true },
+      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    html2pdf().set(opt).from(element).save();
+  });
+});
